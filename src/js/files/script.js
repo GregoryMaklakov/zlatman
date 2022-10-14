97836a02bfc670512835d21137b4c3b7d1d73f5a
Gregory <maklakov.grigory@gmail.com>
2022-10-14 17:34:12 +0200

    ✅burger adapted

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -5,6 +5,14 @@ import { flsModules } from "./modules.js";
 
 document.addEventListener("click", documentActions);
 
+const menuBlocks = document.querySelectorAll('.sub-menu-catalog__block');
+if (menuBlocks.length) {
+	menuBlocks.forEach(menuBlock => {
+		const menuBlockItems = menuBlock.querySelectorAll('.sub-menu-catalog__category').length;
+		menuBlock.classList.add(`sub-menu-catalog__block_${menuBlockItems}`);
+	});
+}
+
 function documentActions(e) {
 	const targetElement = e.target;
 	if (targetElement.closest('[data-parent]')) {
@@ -25,8 +33,24 @@ function documentActions(e) {
 			subMenu.classList.toggle('_sub-menu-open');
 
 		} else {
-			console.log('WTF?')
+			console.log('WTF')
 		}
+		e.preventDefault();
+	}
+    if (targetElement.closest('.menu-top-header__link_catalog')) {
+        document.documentElement.classList.add('catalog-open');
+        e.preventDefault();
+    }
+    if (targetElement.closest('.menu-catalog__back')) {
+		document.documentElement.classList.remove('catalog-open');
+		document.querySelector('._sub-menu-active') ? document.querySelector('._sub-menu-active').classList.remove('_sub-menu-active') : null;
+		document.querySelector('._sub-menu-open') ? document.querySelector('._sub-menu-open').classList.remove('_sub-menu-open') : null;
+		e.preventDefault();
+	}
+    if (targetElement.closest('.sub-menu-catalog__back')) {
+		document.documentElement.classList.remove('sub-menu-open');
+		document.querySelector('._sub-menu-active') ? document.querySelector('._sub-menu-active').classList.remove('_sub-menu-active') : null;
+		document.querySelector('._sub-menu-open') ? document.querySelector('._sub-menu-open').classList.remove('_sub-menu-open') : null;
 		e.preventDefault();
 	}
 }
